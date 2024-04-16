0e96b6d1761b260e99d4ec484fb4cc24e129854a
AsserElshafey <elshafeyasser@yahoo.com>
2024-04-16 20:00:59 +0200

    sparkline

--- dashboard/src/pages/Ecommerce.jsx
+++ dashboard/src/pages/Ecommerce.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BsCurrencyDollar } from 'react-icons/bs';
-import { GoPrimitiveDot } from 'react-icons/go';
+import { GoDotFill } from 'react-icons/go';
 
 import { Stacked, Pie, Button, SparkLine } from '../components';
 
@@ -20,7 +20,7 @@ const Ecommerce = () => {
           <div className='flex justify-between items-center'>
             <div>
               <p className='font-bold text-gray-400'>Earnings</p>
-              <p className='text-2xl text-gray-200'>$49,345,98</p>
+              <p className='text-2xl text-gray-200'>49,345,98 EGP</p>
             </div>
           </div>
           <div className='mt-6'>
@@ -59,8 +59,58 @@ const Ecommerce = () => {
           ))}
         </div>
       </div>
-      <div>
+      <div className='flex gap-10 flex-wrap justify-center'>
+        <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg
+           m-3 p-4 rounded-2xl md:w-780'>
+          <div className='flex justify-between'>
+            <p className='font-semibold text-xl'>Revenue Updates</p>
+            <div className='flex items-center gap-4'>
+              <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                <span><GoDotFill /></span>
+                <span>Expense</span>
+              </p>
+              <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                <span><GoDotFill /></span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+            <div className='border-r-1 border-color m-4 pr-10'>
+              <div>
+                <p>
+                  <span className='text-3xl font-semibold'>60,000 EGP</span>
+                  <span
+                    className='p-1.5 hover:drop-shadow-xl 
+                    cursor-pointer rounded-full text-white
+                   bg-green-400 ml-3 text-xs'>
+                    25%
+                  </span>
+                </p>
+                <p className='text-gray-500 mt-1'>Budget</p>
+              </div>
+              <div className='mt-8'>
+                <p>
+                  <span className='text-3xl font-semibold'>20,000 EGP</span>
 
+                </p>
+                <p className='text-gray-500 mt-1'>Expense</p>
+              </div>
+
+              <div className='mt-5'>
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
