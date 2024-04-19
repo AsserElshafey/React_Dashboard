c6dcbf6eaf19fd6521cc8d31dddcf64b1fb6b90a
AsserElshafey <elshafeyasser@yahoo.com>
2024-04-19 22:07:52 +0200

    final pages

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
-import { FiShoppingCart } from 'react-icons/fi';
 import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import avatar from '../data/avatar.jpg';
-import { Cart, Chat, Notification, UserProfile, } from '.';
+import { Chat, Notification, UserProfile, } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
@@ -26,7 +25,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 const Navbar = () => {
   const { activeMenu, setActiveMenu,
-    isClicked, setIsClicked,
+    isClicked,
     screenSize, setScreenSize,
     handleClick, currentColor } = useStateContext();
 
@@ -45,24 +44,17 @@ const Navbar = () => {
     }
   }, [screenSize])
 
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
     <div className='flex justify-between p-2 md:mx-6 relative'>
       <NavButton
         title="Menu"
-        customFunc={() => setActiveMenu(
-          (prevActiveMenu) => !prevActiveMenu
-        )}
+        customFunc={handleActiveMenu}
         color={currentColor}
         icon={<AiOutlineMenu />}
       />
       <div className='flex'>
-        <NavButton
-          title="Cart"
-          customFunc={() => handleClick('cart')}
-          color={currentColor}
-          icon={<FiShoppingCart />}
-        />
         <NavButton
           title="Chat"
           dotColor="#03C9D7"
@@ -93,7 +85,6 @@ const Navbar = () => {
             <MdKeyboardArrowDown className='text-gray-400 text-14' />
           </div>
         </TooltipComponent>
-        {isClicked.cart && <Cart />}
         {isClicked.chat && <Chat />}
         {isClicked.notification && <Notification />}
         {isClicked.userProfile && <UserProfile />}
